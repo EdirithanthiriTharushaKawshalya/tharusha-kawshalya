ca2faf7df455b115777c78be6066a70c491a7126
Tharusha Kawshalya <tharusha.k.dev@gmail.com>
2025-12-26 22:24:59 +0530

    updated: title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/layout/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Kawshalya | Software Engineer",
+  title: "Edirithanthiri Tharusha Kawshalya",
   description: "Edirithanthiri Tharusha Kawshalya",
 };
 
